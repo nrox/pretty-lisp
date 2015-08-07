edf618dd5d3cdf7281ff8db2072198a9df7cc95a
nrox <nunuroxa@gmail.com>
2015-08-07 15:51:45 +0200

    start structure for  solver::solve

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 PORT=${1:-4555}
-google-chrome "http://localhost:$PORT" && sbcl --load package.lisp --eval "( pretty-lisp:up :port $PORT)"
+google-chrome "http://localhost:$PORT" && sbcl --load package.lisp
+#--eval "( pretty-lisp:up :port $PORT)"
 exit 0
